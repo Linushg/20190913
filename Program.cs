6c4b8493f0b042d29e1becb17fc7d58033b44a68
Linus Gyllenberg <linus@sbgdata.se>
2019-09-17 15:38:21 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var v = 1;
+
+            Console.Write("Skriv en mening");
+            Console.ReadLine();
+
+            Console.WriteLine("Det är " , v);
+        }
+    }
+}
